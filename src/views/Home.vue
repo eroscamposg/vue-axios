2bde763f430c748c94a46c2e7a18425fa88e4db0
Eros Campos <eroscamposg@gmail.com>
2020-01-03 21:30:32 -0500

    Adding date and some vuetify stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-layout>
+      <v-flex xs12>
+        <v-card>
+          <v-row>
+            <v-date-picker v-model="date" full-width></v-date-picker>
+          </v-row>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-sm-center">
+            650 - {{ date }}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: {},
+  data() {
+    return {
+      date: ""
+    };
   }
-}
+};
 </script>
